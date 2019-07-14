36760b87931fd901a0dce8331549c64f45621ca0
Victor M. Suarez <svmnotn@gmail.com>
2019-07-14 02:41:44 -0400

    feat: publish docs as gh-pages for now
    This is so that there is no need to download the projects to see how the docs are
    
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

---
+++ docs/truth_tester/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["parsing","Here are all the Boolean Expression Parsing  data structures"]],"struct":[["State","The state of a [`Tester`]"],["Tester","A struct used to store both  the user given expression, and the [`State`] of that expression."]],"traitalias":[["ExprFn","A function that takes a [`State`] and returns a [`bool`]"]]});
